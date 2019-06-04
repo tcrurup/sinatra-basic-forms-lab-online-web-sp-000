97bd78ae24da02ed6fe1986d4cf8bedb791eda0e
Anthony Rurup <tcrurup@gmail.com>
2019-06-04 23:31:48 +0000

    Automatically backed up by Learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -1,7 +1,7 @@
 class Puppy
   
-  attr_reader :name, :breed
-  attr_accessor :months_old
+  attr_reader :breed
+  attr_accessor :months_old, :name
   
   
   def initialize(name, breed, months_old)
