c9b22f90d07dbe45961c807e21ce1896524e9ca6
Anthony Rurup <tcrurup@gmail.com>
2019-06-04 23:50:27 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -12,7 +12,7 @@ class App < Sinatra::Base
 
   post '/puppy' do
     puts params
-    @puppy = Puppy.new(params)
+    @puppy = Puppy.new(params[:name], params[:breed], params[:age])
     erb :display_puppy
   end
 end
